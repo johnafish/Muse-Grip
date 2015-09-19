f7a97a9a9dbdab1ebe53f3930eb4bdfce501a1ce
John Fish <john@johnafish.ca>
2015-09-19 00:40:00 -0400

    Testing blink vs jaw clench

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ parser.add_argument("--port",type=int, default=1337, help="The port to listen on
 args = parser.parse_args()
 dispatcher = dispatcher.Dispatcher()
 dispatcher.map("/muse/elements/blink", blinkHandler)
+dispatcher.map("/muse/elements/jaw_clench", blinkHandler)
 server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
 server_thread = threading.Thread(target=server.serve_forever)
 server_thread.start()
