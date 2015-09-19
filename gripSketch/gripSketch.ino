7d7f4f9ce6b54edbf22d69f33160ae8c74a2c8dc
John Fish <john@johnafish.ca>
2015-09-19 02:03:12 -0400

    Arduino sketch reacts to serial input
    LED blinks once detected input from python.

--- gripSketch/gripSketch.ino
+++ gripSketch/gripSketch.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  serialEvent();
 }
+
+void serialEvent() {
+  int readValue = Serial.read();
+  Serial.println(readValue);
+  if (readValue==1){
+    blinker();
+  } else {
+    digitalWrite(13,LOW);
+  }
+}
+
+void blinker(){
+  for (int i=0;i<2;i++){
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+  }
+}
+
