64a7eaf197fea3e2c1e179488dd8f829036c92d7
John Fish <john@johnafish.ca>
2015-09-19 00:34:57 -0400

    Basic osc setup

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
+import argparse
+
 from pythonosc import dispatcher
 from pythonosc import osc_server
+
+
+# Thanks osc_server on github for this code
+global dispatcher, Dispatcher, server
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="localhost", help="The ip to listen on")
+parser.add_argument("--port",type=int, default=1337, help="The port to listen on")
+args = parser.parse_args()
