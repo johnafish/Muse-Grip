122ccb8629641a1600dd4e5a79c460613b9e7c23
John Fish <john@johnafish.ca>
2015-09-19 01:17:23 -0400

    Decided on clench, timestamps

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import argparse
 import threading
+import time
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+lastValue = 0
+lastTime = 0
 
-def blinkHandler(*data):
-	print(data)
+def clenchHandler(*data):
+	handleType, value = data
+	print(value)
+	print(time.time())
 
 # Thanks osc_server on github for this code
 global dispatcher, Dispatcher, server
@@ -15,8 +20,7 @@ parser.add_argument("--ip", default="localhost", help="The ip to listen on")
 parser.add_argument("--port",type=int, default=1337, help="The port to listen on")
 args = parser.parse_args()
 dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/muse/elements/blink", blinkHandler)
-dispatcher.map("/muse/elements/jaw_clench", blinkHandler)
+dispatcher.map("/muse/elements/jaw_clench", clenchHandler)
 server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
 server_thread = threading.Thread(target=server.serve_forever)
 server_thread.start()
