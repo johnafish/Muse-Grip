9adc17ecd5c2bc4f940d0bad4b216dbe02e3ce15
John Fish <john@johnafish.ca>
2015-09-19 00:37:25 -0400

    Blink handler in server

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import argparse
+import threading
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+
+def blinkHandler(*data):
+	print(data)
 
 # Thanks osc_server on github for this code
 global dispatcher, Dispatcher, server
@@ -10,3 +14,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--ip", default="localhost", help="The ip to listen on")
 parser.add_argument("--port",type=int, default=1337, help="The port to listen on")
 args = parser.parse_args()
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/muse/elements/blink", blinkHandler)
+server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+server_thread = threading.Thread(target=server.serve_forever)
+server_thread.start()
