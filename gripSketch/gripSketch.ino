c5b6aafe5928814e3a489033c8934956f4d81934
John Fish <john@johnafish.ca>
2015-09-19 11:29:21 -0400

    Now toggles servos on serial reading
    Once it gets input from python it toggles the opening/closing of servos.

--- gripSketch/gripSketch.ino
+++ gripSketch/gripSketch.ino
@@ -1,7 +1,17 @@
+#include <Servo.h>
+
+Servo servoOne;
+Servo servoTwo;
+Servo servoThree;
+
+boolean opened = true;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  servoOne.attach(7);
+  servoTwo.attach(8);
+  servoThree.attach(9);
+  openServos();
 }
 
 void loop() {
@@ -12,18 +22,31 @@ void serialEvent() {
   int readValue = Serial.read();
   Serial.println(readValue);
   if (readValue==1){
-    blinker();
+    toggle();
   } else {
     digitalWrite(13,LOW);
   }
 }
 
-void blinker(){
-  for (int i=0;i<2;i++){
-    digitalWrite(13, HIGH);
-    delay(500);
-    digitalWrite(13, LOW);
-    delay(500);
+void toggle(){
+  if (opened == true){
+    closeServos();
+    opened = false;
+  } else {
+    openServos();
+    opened = true;
   }
+}
+
+void closeServos(){
+  servoOne.write(25);
+  servoTwo.write(25);
+  servoThree.write(25);
+}
+
+void openServos(){
+  servoOne.write(0);
+  servoTwo.write(0);
+  servoThree.write(0);
 }
 
